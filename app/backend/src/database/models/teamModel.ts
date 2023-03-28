798c00eea33b854d8747f642a9fc9013e3a2da1b
João <joaolelles7@gmail.com>
2023-03-28 20:04:35 -0300

    Requisito 1

---
+++ app/backend/src/database/models/teamModel.ts
@@ -0,0 +1,29 @@
+import { INTEGER, Model, STRING } from 'sequelize';
+import db from '.';
+// import OtherModel from './OtherModel';
+
+class Teams extends Model {
+  declare id: number;
+  declare teamName: string;
+}
+
+Teams.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: INTEGER,
+  },
+  teamName: {
+    field: 'team_name',
+    allowNull: false,
+    type: STRING,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
+
+export default Teams;
